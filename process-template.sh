be07f119bcd27e4964b959da70b95ddefc617584
Buck Doyle <b@chromatin.ca>
2017-04-06 15:36:05 -0700

    Add second update SQL for .com database

--- process-template.sh
+++ process-template.sh
@@ -14,4 +14,5 @@ fi
 
 export REPLACEMENT_STRING="${END_DAYS_FROM_NOW} day${PLURAL_DAYS}"
 
-sed "s/REPLACE/$REPLACEMENT_STRING/" template.sql > update.sql
+sed "s/REPLACE/$REPLACEMENT_STRING/" template.sql > update-org.sql
+sed "s/3544/4106/" update-org.sql > update-com.sql
